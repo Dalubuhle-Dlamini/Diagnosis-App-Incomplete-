a7d3fa2ee259f847b06f1165758cc90256e98719
Sihlelo Dalubuhle Dlamini <68609913+Dalubuhle-Dlamini@users.noreply.github.com>
2024-06-06 14:37:14 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,48 @@
 from flask import Flask, render_template, request, url_for, redirect
 import pandas as pd
-from dao.trainModel import train_model
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.metrics import accuracy_score
+import pickle
+
 from dao.processData import process_input_data
 
 app = Flask(__name__)
+
+def train_model():
+    data = pd.read_csv('./datasets/common-diseases.csv')
+    X = data.drop('Disease', axis=1)
+    y = data['Disease']
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.3, random_state=42)
+
+    categorical_cols = ['Gender']
+    label_encoders = {}
+
+    for col in categorical_cols:
+        le = LabelEncoder()
+        X_train[col] = le.fit_transform(X_train[col])
+        X_test[col] = le.transform(X_test[col])
+        label_encoders[col] = le
+
+    onehot_encoder = OneHotEncoder(handle_unknown='ignore')
+    X_train_encoded = onehot_encoder.fit_transform(X_train)
+    X_test_encoded = onehot_encoder.transform(X_test)
+
+    rf_classifier = RandomForestClassifier(n_jobs=2, random_state=42)
+    rf_classifier.fit(X_train_encoded, y_train)
+
+    y_pred = rf_classifier.predict(X_test_encoded)
+    accuracy = accuracy_score(y_test, y_pred) * 100
+
+    with open('model.pkl', 'wb') as model_file:
+        pickle.dump(rf_classifier, model_file)
+        pickle.dump(onehot_encoder, model_file)
+        pickle.dump(label_encoders, model_file)
+        pickle.dump(accuracy, model_file)
+
 
 train_model()
 
